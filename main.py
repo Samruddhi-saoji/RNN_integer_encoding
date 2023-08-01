f68c3887cb1f0c7553bc81ffef505f49f8e4d61d
Samruddhi-saoji <90205915+Samruddhi-saoji@users.noreply.github.com>
2023-08-02 01:10:39 +0530

    commit

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-#sentiment analysis on imdb dataset
-
 #import libraries
 from keras.datasets import imdb
 from keras.utils import pad_sequences
@@ -9,25 +7,21 @@ from keras.layers import SimpleRNN, Dense
 # load the IMDB dataset
 (x_train, y_train), (x_test, y_test) = imdb.load_data()
 
-# padding to make all reviews of same length (w)
-w = 30 #max number of words
+#padding
+w = 30 #all reviews should have exactly "w" words
 x_train = pad_sequences(x_train, maxlen=w)
 x_test = pad_sequences(x_test, maxlen=w)
 
-# Build the RNN model
+#Build the RNN model
 model = Sequential()
 
 #add the layers
-# input = 1 sentence  #shape = (w, 1)
+#input shape = (w, 1)
 model.add(SimpleRNN(32, input_shape=(w,1), return_sequences=False)) #recurrent layer with 32 neurons
 model.add(Dense(1,activation='sigmoid')) #output layer with 1 neuron    
 
 #compile the model
-model.compile(
-    loss='binary_crossentropy',
-    optimizer='adam',
-    metrics=['accuracy']
-)
+model.compile( loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 # training and testing
 epochs = 5
